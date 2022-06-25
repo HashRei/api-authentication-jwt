2c6595f18aa2b1cc82e4d22b3d74e00d6c4c98f4
HashRei <hashrei@protonmail.com>
2022-06-25 20:51:09 +0200

    Add user validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { registerValidation } = require("../validation");
+
 
 router.post("/request", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const post = new User({
     name: req.body.name,
     email: req.body.email,
@@ -17,4 +22,3 @@ router.post("/request", async (req, res) => {
 });
 
 module.exports = router;
-
