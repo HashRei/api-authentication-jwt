4c8fab4dca8fd0ec234362f1c5248c02957f89a7
HashRei <hashrei@protonmail.com>
2022-06-25 14:21:07 +0200

    Add middlewares and connect to database

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const authRoute = require("./routes/auth");
+const mongoose = require("mongoose")
+require("dotenv/config");
 
 const app = express();
 
-app.listen(3000, () => console.log("Up and running"));
+// Middlewares
+app.use("/api/user", authRoute);
+
+
+// Connect to database
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log("Connected to my Mongo DB!!!!!!!!!");
+  });
+
+app.listen(3000, () => console.log("Server is running on port: 3000"));
