399854419bbc38a17e1db34d0b0767057a875506
HashRei <hashrei@protonmail.com>
2022-06-25 17:43:38 +0200

    Add user registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.post("/request", (req, res) => {
-  res.send("Register");
+router.post("/request", async (req, res) => {
+  const post = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedPost = await post.save(); // Save the entered post to the database
+    res.json(savedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
-  
-
 module.exports = router;
+
