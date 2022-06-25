b02ac89182c623c844c979ff16a580e63dc236bc
HashRei <hashrei@protonmail.com>
2022-06-25 17:42:56 +0200

    Add body parser to perform post requests

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const authRoute = require("./routes/auth");
 const mongoose = require("mongoose")
+// const bodyParser = require("body-parser");
 require("dotenv/config");
 
 const app = express();
 
-// Middlewares
-app.use("/api/user", authRoute);
-
-
 // Connect to database
 mongoose.connect(process.env.DB_CONNECTION, () => {
-    console.log("Connected to my Mongo DB!!!!!!!!!");
-  });
+  console.log("Connected to my Mongo DB!!!!!!!!!");
+});
+
+// Middlewares
+// app.use(bodyParser.json());
+app.use(express.json()) // This is a body parser, ables post requests
+app.use("/api/user", authRoute);
 
 app.listen(3000, () => console.log("Server is running on port: 3000"));
